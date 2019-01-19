67814869cae4a84fbb85c41dc0dd08ee6f96b784
moneystack1 <moneystack1@users.noreply.github.com>
2019-01-19 16:23:57 -0500

    updated init_git.sh

--- init_git.sh
+++ init_git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config --replace-all user.name "moneystack"
-git config --replace-all user.email "alex@moneystack.com"
-ssh-add ~/.ssh/moneystack.id_rsa
+git config --replace-all user.name "moneystack1"
+git config --replace-all user.email "moneystack1@users.noreply.github.com"
+ssh-add ~/.ssh/moneystack1.id_rsa
 
